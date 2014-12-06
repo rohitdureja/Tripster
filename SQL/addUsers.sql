00ac3da8b6cd80fcd7031349322d5c546bb0631a
Rohit Dureja <rohit@seas.upenn.edu>
2014-12-06 18:45:48 -0500

    6-Dec

--- SQL/addUsers.sql
+++
@@ -1,6 +0,0 @@
-INSERT ALL
-	INTO Users (username, firstname, lastname) VALUES ('rohitdureja', 'Rohit', 'Dureja')
-	INTO Users (username, firstname, lastname) VALUES ('rahuldureja', 'Rahul', 'Dureja')
-	INTO Users (username, firstname, lastname) VALUES ('pinky.dureja', 'Pinky', 'Dureja')
-	INTO Users (username, firstname, lastname) VALUES ('nk.dureja', 'Naresh Kumar', 'Dureja')
-SELECT * FROM dual;
--- SQL/createtable.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Users (
-	username 	VARCHAR2(50) PRIMARY KEY,
-	firstname	VARCHAR2(20),
-	lastname	VARCHAR2(20)
-	);
