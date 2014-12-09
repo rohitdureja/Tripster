ce215685ad2386725540f3d93d843737ed5a6389
Rishabh Gulati <rgulati@seas.upenn.edu>
2014-12-08 22:39:04 -0500

    add friend to trip

---
+++ SQL/add_friend_to_trip.sql
@@ -0,0 +1,2 @@
+/*INSERT INTO TRIPS_USERS VALUES ('trip_id', 'user_to_be_added', NULL, 'Pending', 'person_initiating the request_current user')*/
+INSERT INTO TRIPS_USERS VALUES ('1000', '1001', NULL, 'Pending', '1000');
