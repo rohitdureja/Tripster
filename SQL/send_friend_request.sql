8bbbca4028813aa33f897942f5172ad1eb4baf23
Rishabh Gulati <rgulati@seas.upenn.edu>
2014-12-08 22:09:42 -0500

    added user trips query

--- SQL/send_friend_request.sql
+++ SQL/send_friend_request.sql
@@ -1,1 +1,2 @@
-INSERT INTO FRIENDS VALUES ('1000', '1', 'Pending');
+/*1st id : user initiating the friend request, 2nd id is for the person being requested*/
+INSERT INTO FRIENDS VALUES ('1000', '1001', 'Pending');
