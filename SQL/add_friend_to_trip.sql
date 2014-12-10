09050d8864f6285e4dc3b2720643b39c2cf064af
Rishabh Gulati <rgulati@seas.upenn.edu>
2014-12-09 23:21:52 -0500

    updated query friends for current trip with status

--- SQL/add_friend_to_trip.sql
+++ SQL/add_friend_to_trip.sql
@@ -1,2 +1,2 @@
 /*INSERT INTO TRIPS_USERS VALUES ('trip_id', 'user_to_be_added', NULL, 'Pending', 'person_initiating the request_current user')*/
-INSERT INTO TRIPS_USERS VALUES ('1000', '1001', NULL, 'Pending', '1000');
+INSERT INTO TRIPS_USERS VALUES ('1045', '6', NULL, 'Pending', '1000');
