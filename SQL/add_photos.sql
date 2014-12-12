3ccb78401034c13ab76e63cd48c6893e823777c8
Rishabh Gulati <rgulati@seas.upenn.edu>
2014-12-12 17:09:08 -0500

    added trip feed query for fetching photos and videos for a trip with likes

---
+++ SQL/add_photos.sql
@@ -0,0 +1,14 @@
+/*sequence photo id, album id, tagline, privacy, date, url*/
+INSERT INTO PHOTOS VALUES (1004, 103, 'Morning bliss', 1, sysdate, 'http://www.metromba.com/wp-content/uploads/2013/02/la-salle-library-Audrey-Wiki.jpg');
+
+/*
+SELECT sequence_name from user_sequences;
+
+create sequence albumid_seq?  
+increment by 1?
+start with 1000
+*/
+
+
+  
+
